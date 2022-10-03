95311c67b15955d1fb676d9251f5c234f38e6bc4
Ishan Nethmina <91957356+Mr-Ghost001@users.noreply.github.com>
2022-10-03 09:59:47 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -271,24 +271,3 @@
     }
 
 
-var clicks = 0;
-
-function initiateClicks() {
-    var clickStr = localStorage.getItem("clicks");
-    if(clickStr == undefined){
-        localStorage.setItem("clicks", 0);
-        clicks = 0;
-    }else{
-        clicks = parseInt(clickStr);   
-    }
-    document.getElementById("clicks").innerHTML = clicks;
-}
-
-function doClick() {
-    clicks += 1;
-    localStorage.setItem("clicks", clicks);
-    document.getElementById("clicks").innerHTML = clicks;
-}
-
-document.getElementById("clickButton").onclick = doClick;
-initiateClicks();
